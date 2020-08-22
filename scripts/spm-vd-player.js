714a6257287602cbc6178881e0fc2fb2026aed16
Ivan <root.class@mail.ru>
2020-08-22 16:58:00 +0300

    человек-паук
    Добавлен новый фильм "Человек Паук: Возвращение Домой"
    Убран Предпросмотр фильмов из-за неполадок в работе с ним

---
+++ scripts/spm-vd-player.js
@@ -0,0 +1,1 @@
+var player = new Playerjs({id:"player", "title":"Человек-Паук (2017)", "file":"https://cdn34.my.mail.ru/hv/63131825.mp4?slave%5B%5D=s%3Ahttp%3A%2F%2Fvideo-cephrgw1.i%3A9090%2Fvideo4%2F63131825-hv&p=f&expire_at=1598115600&touch=1506573017&reg=188&sign=4c489b550ef28d045d39f611197c9a34de670673"})
